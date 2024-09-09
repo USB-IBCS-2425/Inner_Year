54bfab5eda5e7d232a101e473794f820f08a1a80
Thomas Sands <thomas_sands@m018896.aw.org>
2024-09-09 12:57:38 -0700

    problem set from before github setup

---
+++ 2024-09-04-java-problem-set-1/ps1.java
@@ -0,0 +1,15 @@
+// Java Problem Set 1 2024.09.04
+
+class ps1 {
+	public static void main(String[] args) {
+		System.out.print("Thomas is a student in IB CS.");
+		System.out.print("\n\na) Java is compiled whereas Python is interpreted. Python is also arguably simpler in terms of syntax, making it well-suited for learning to program.");
+		System.out.print("\n\nb) High level languages are designed to be read by humans, and are largely abstracted from the actual operations the computer is preforming. Assembly languages, on the other hand, correspond much more closely to the individual machine instructions of the computer.");
+		System.out.print("\n\nc) A compiler converts a higher level programmng language into a lower level language.");
+	}
+}
+
+
+
+
+
